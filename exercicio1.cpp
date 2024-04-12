f26c1a92d4b946295205a4e16b12b743fc75b82b
maarianavalentim <valentimmariana26@gmail.com>
2024-04-12 15:11:35 -0300

    Initial commit

---
+++ exercicio1.cpp
@@ -0,0 +1,97 @@
+
+/*#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    const double precoInstalacao = 300.00;
+    const double precoLimpeza = 150.00;
+    const double precoManutencao = 499.99;
+
+
+    double total = precoInstalacao + precoLimpeza + precoManutencao;
+
+
+    cout << "O cliente devera pagar: R$" << total << endl;
+
+    return 0;
+}*/
+
+/*#include <iostream>
+
+using namespace std;
+
+int main() {
+   
+    double salarioPorHora = 35.00;
+    int horasPorDia = 8;
+
+  
+    double salarioDiario = salarioPorHora * horasPorDia;
+
+    
+    cout << "Ao final do dia, Joaquim deve receber R$" << salarioDiario << "." << endl;
+
+    return 0;
+}*/
+
+/*#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Definindo o número de unidades em cada caixa e o número de caixas compradas por Pamela
+    int unidadesPorCaixa = 27;
+    int caixasCompradas = 16;
+
+    // Calculando o total de bombons que Pamela tem
+    int totalBombons = unidadesPorCaixa * caixasCompradas;
+
+    // Exibindo o resultado
+    cout << "Pamela tem um total de " << totalBombons << " bombons." << endl;
+
+    return 0;
+}*/
+
+/*#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int totalGarrafasVendidas = 300;
+    int garrafasPorCaixa = 20;
+
+    
+    int caixasUsadas = totalGarrafasVendidas / garrafasPorCaixa;
+
+   
+    cout << "A loja usou um total de " << caixasUsadas << " caixas de água." << endl;
+
+    return 0;
+}*/
+
+/*#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Definindo as variáveis
+    int camisetasCompradasInicialmente = 120;
+    int camisetasVendidas = 80;
+    int precoCamiseta = 30;
+    int camisetasCompradasNovamente = 50;
+
+    // Calculando o total de camisetas que a loja tem agora
+    int totalCamisetas = camisetasCompradasInicialmente - camisetasVendidas + camisetasCompradasNovamente;
+
+    // Calculando o faturamento total
+    int faturamento = (camisetasCompradasInicialmente - camisetasVendidas) * precoCamiseta;
+
+    // Exibindo os resultados
+    cout << "A loja tem agora " << totalCamisetas << " camisetas." << endl;
+    cout << "O faturamento total foi de R$" << faturamento << "." << endl;
+
+    return 0;
+}*/
